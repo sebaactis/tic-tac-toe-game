dfc6ed3fa01468d92e6e3ce48e47672e41617840
sebastian.actis <sebaactis@gmail.com>
2023-01-31 15:01:40 -0300

    logica de ganador y render del tablero

--- src/App.js
+++ src/App.js
@@ -28,16 +28,42 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+// Combinaciones ganadores
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 
 function App() {
 
   const [board, setBoard] = useState(Array(9).fill(null)) // Tablero
   const [turn, setTurn] = useState(TURNS.X) // Turnos
+  const [winner, setWinner] = useState(null) // Ganador
+
+
+  // Funcion que verifica el ultimo tablero y verifica si coincide con las combos ganadoras
+  const checkWinner = (boardToCheck) => {
+    for (const combo of WINNER_COMBOS) {
+      const [a, b, c] = combo
+      if (boardToCheck[a] && boardToCheck[a] === boardToCheck[b] && boardToCheck[a] === boardToCheck[c]) {
+        return boardToCheck[a]
+      }
+    }
+    return null;
+  }
 
 
   // Funcion que actualiza el tablero
   const updateBoard = (index) => {
 
+    if (board[index] || winner) return // Verifica si hay algo en esa posicion
 
     // Actualiza el tablero con los clicks
     const newBoard = [...board]
@@ -47,6 +73,12 @@ function App() {
     // Cambia de turno segun el state actual de turn
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
+
+    // Verifica si hay un nuevo ganador y setea el estado winner
+    const newWinner = checkWinner(newBoard)
+    if(newWinner) {
+      setWinner(newWinner)
+    }
 
   }
 
