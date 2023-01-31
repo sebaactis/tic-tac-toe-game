2694afad0d566ae5b9dd8a8069148faef1e813c6
sebastian.actis <sebaactis@gmail.com>
2023-01-31 14:49:22 -0300

    primer version del juego

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import { useState } from 'react';
 import './App.css';
 
+// Objeto de turnos
+
 const TURNS = {
   X: "X",
   O: "O"
 };
 
 
+// Componente que renderiza los cuadrados del tablero
 
 const Square = ({ children, isSelected, updateBoard, index }) => {
 
   const className = `square ${isSelected ? 'is-selected' : ''}`
 
+  // Funcion que maneja el evento de actualizar el tablero
   const handleClick = () => {
-    updateBoard();
+    updateBoard(index);
   }
+
 
   return (
     <div onClick={handleClick} className={className}>
@@ -26,17 +31,26 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
 
 function App() {
 
-  const [board, setBoard] = useState(Array(9).fill(null))
-  const [turn, setTurn] = useState(TURNS.X)
+  const [board, setBoard] = useState(Array(9).fill(null)) // Tablero
+  const [turn, setTurn] = useState(TURNS.X) // Turnos
 
-  const updateBoard = () => {
+
+  // Funcion que actualiza el tablero
+  const updateBoard = (index) => {
+
+
+    // Actualiza el tablero con los clicks
+    const newBoard = [...board]
+    newBoard[index] = turn // Donde se da click se pone el X / O segun el state turn
+    setBoard(newBoard)
+
+    // Cambia de turno segun el state actual de turn
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
     setTurn(newTurn)
 
-    
   }
 
-
+  // Render del tablero
   return (
     <main className="board">
       <h1> TIC TAC TOE</h1>
